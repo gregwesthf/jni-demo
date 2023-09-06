051b559aa890992b5402d99d091e68d51cfc4ab4
Greg West <greg.west@headforwards.com>
2023-09-06 10:26:48 +0100

    add button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+function send() {
+  console.log("send");
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="content">
+        <h1 className="title">
+          Email Demo
+        </h1>
+        <button className="btn" onClick={send}>Send</button>
+      </div>
     </div>
   );
 }
